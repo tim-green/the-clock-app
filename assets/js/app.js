1be610465b53fcc56b3dba995e139428027ab248
Tim <github@timgreen.ws>
2023-08-14 13:04:57 +1000

    added constants for clock

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+//const
+const background = document.querySelector('.background')
+const author = document.querySelector('.author')
+const icon = document.querySelector('.icon')
+const details = document.querySelector('.details');
+const period = document.querySelector('.period');
+const expand = document.querySelector('.expand');
+
