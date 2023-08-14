d456b16dccd92915b63793224436ba26135648a6
Tim <github@timgreen.ws>
2023-08-14 15:59:03 +1000

    added const for author, period

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
+
 //const
 const background = document.querySelector('.background')
-const author = document.querySelector('.author')
+const author = document.querySelector(".author")
 const icon = document.querySelector('.icon')
 const details = document.querySelector('.details');
-const period = document.querySelector('.period');
+const period = document.querySelector(".period");
 const expand = document.querySelector('.expand');
 
 // function to get quotefunction getQuote() {    axios.get('https://api.quotable.io/random').then((quotesRes) => {    const chosenQuote = quotesRes.data      document.getElementById("quote").textContent = chosenQuote.content;            if (chosenQuote.author == null) {          author.textContent = 'Unknown author'      } else {          author.textContent = chosenQuote.author;      }    }).catch((err) => console.error(err))  }
