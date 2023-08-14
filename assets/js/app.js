a562ffdf2103dddc38c5900fadc6583c72052a36
Tim <github@timgreen.ws>
2023-08-14 16:00:34 +1000

    added event listeners for app

--- assets/js/app.js
+++ assets/js/app.js
@@ -109,3 +109,19 @@ function getLocation() {
   getTimeZone();
   getLocation()
 
+//   the event listeners
+function showDetails() {
+    document.querySelector('.top-widgets').classList.toggle('transform');
+    details.classList.toggle('transform');
+    
+    if (expand.firstChild.nodeValue === "More") {
+      expand.firstChild.nodeValue = "Less"
+    } else {
+      expand.firstChild.nodeValue = "More"
+    }
+  
+    const arrow = document.querySelector('.arrow');
+    arrow.classList.toggle('rotate');
+  }
+  expand.addEventListener('click', showDetails);
+
