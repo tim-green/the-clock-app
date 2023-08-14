76bf365242dd0e5090f5941601e28e996f3abbe9
Tim <github@timgreen.ws>
2023-08-14 15:59:17 +1000

    added function to get quote

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,4 +7,18 @@ const details = document.querySelector('.details');
 const period = document.querySelector(".period");
 const expand = document.querySelector('.expand');
 
-// function to get quotefunction getQuote() {    axios.get('https://api.quotable.io/random').then((quotesRes) => {    const chosenQuote = quotesRes.data      document.getElementById("quote").textContent = chosenQuote.content;            if (chosenQuote.author == null) {          author.textContent = 'Unknown author'      } else {          author.textContent = chosenQuote.author;      }    }).catch((err) => console.error(err))  }
+// function to get quotefunction getQuote() {    axios.get('https://api.quotable.io/random').then((quotesRes) => {    const chosenQuote = quotesRes.data      document.getElementById("quote").textContent = chosenQuote.content;            if (chosenQuote.author == null) {          author.textContent = 'Unknown author'      } else {          author.textContent = chosenQuote.author;      }    }).catch((err) => console.error(err))  }// function to get quote
+function getQuote() {
+    axios.get('https://api.quotable.io/random').then((quotesRes) => {
+    const chosenQuote = quotesRes.data
+  
+    document.getElementById("quote").textContent = chosenQuote.content;
+      
+      if (chosenQuote.author == null) {
+          author.textContent = 'Unknown author'
+      } else {
+          author.textContent = chosenQuote.author;
+      }
+    }).catch((err) => console.error(err))
+  }
+
